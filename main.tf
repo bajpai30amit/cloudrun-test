da52854169b8a429490a74eff38aa441e8b6285c
bajpai30amit <98395683+bajpai30amit@users.noreply.github.com>
2022-11-17 15:22:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,31 @@
-provider "google" {
-  credentials = file("ttec-test.json")
-  project = "noc-test-project"
-  region  = "us-central1"
-  zone    = "us-central1-a"
+terraform {
+  required_version = ">= 1.1.2"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.1.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.2.0"
+    }
+  }
 }
 
+provider "google" {
+  project     = "noc-test-project"
+  region      = "us-central-1"
+  credentials = file("66-test.json")
+}
 
 resource "google_cloud_run_service" "default" {
-  name     = "demo"
+  name     = "cloudrun-srv"
   location = "us-central1"
 
   template {
     spec {
       containers {
-        image = "gcr.io/noc-test-project/hello-dotnet"
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
       }
     }
   }
@@ -22,4 +34,20 @@ resource "google_cloud_run_service" "default" {
     percent         = 100
     latest_revision = true
   }
+}
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
